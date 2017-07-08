3d1dea3bc5e6df51b1d837cdc369d6d35aadcbf5
Prashant Chaurasia <prashant.chaurasia1311@gmail.com>
2017-07-09 01:04:34 +0530

    Added markdown support , Custom Template Tags and Filters

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     #
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'
         r'(?P<post>[-\w]+)/$',views.post_detail,name='post_detail'),
+    url(r'tag/(?P<tag_slug>[-\w]+)/$',views.post_list,name='post_list_by_tag'),
 ]
