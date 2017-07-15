f486bf5867643311b6d1c22973c8897ae88f6098
Prashant Chaurasia <prashant.chaurasia1311@gmail.com>
2017-07-16 02:53:58 +0530

    Added nav-bar

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     #starting page home /blog/
     url(r'^$',views.post_list,name = 'post_list'),
-    #
+
+url(r'tag/(?P<tag_slug>[-\w]+)/$',views.post_list,name='post_list_by_tag'),
+
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'
         r'(?P<post>[-\w]+)/$',views.post_detail,name='post_detail'),
-    url(r'tag/(?P<tag_slug>[-\w]+)/$',views.post_list,name='post_list_by_tag'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -26,7 +26,7 @@ def post_list(request, tag_slug=None):
     return render(request,'blog/post/list.html',{'page':page,'posts':posts,'tag':tag})
 
 def post_detail(request,year,month,day,post):
-    post = get_object_or_404(Post,slug=post,status='published',publish__year=year
+    post = get_object_or_404(Post, slug=post,status='published',publish__year=year
                              ,publish__month=month,publish__day=day)
     # list pf active comments for this post
     comments = post.comments.filter(active = True)
